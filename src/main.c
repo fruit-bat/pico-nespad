a35eeebf690af8524a0d0cb16e42c27f2c46a683
Phil <pmscull@gmail.com>
2024-09-07 11:28:37 +0100

    WIP

--- src/main.c
+++ src/main.c
@@ -4,70 +4,9 @@
 #include "hardware/gpio.h"
 #include "hardware/sync.h"
 #include "hardware/clocks.h"
-#include "nespad.pio.h"
+#include "nespad.h"
 #include "pico/stdio_uart.h"
 
-static PIO _pio;
-static uint _sm;
-static uint32_t _nespad_state = 0;
-
-static void __not_in_flash_func(nespad_isr)() {
-  while (!pio_sm_is_rx_fifo_empty(_pio, _sm)) {
-    _nespad_state = ~_pio->rxf[_sm];
-  }  
-}
-
-// It may be more sensible to run much slower than the original clock of 83.3kHz (12e-6s)
-// 17kHz (59e-6s) would give 1 reading per millisecond
-#ifndef NESPAD_CLOCK_PERIOD_S
-#define NESPAD_CLOCK_PERIOD_S 59e-6
-#endif 
-
-static void nespad_program_init(PIO pio, uint sm, uint data_pin_base, uint clock_pin_base) {
-  _pio = pio;
-  _sm = sm;
-  printf("Adding program\n");
-
-  uint offset = pio_add_program(pio, &nespad_program);
-
-  printf("Setting up GPIO\n");
-
-  pio_gpio_init(pio, data_pin_base + 0);
-  pio_gpio_init(pio, data_pin_base + 1);
-  pio_gpio_init(pio, clock_pin_base + 0);
-  pio_gpio_init(pio, clock_pin_base + 1);
-  gpio_set_pulls(data_pin_base, true, false);
-  gpio_set_pulls(data_pin_base+1, true, false);
-
-  pio_sm_config c = nespad_program_get_default_config(offset);
-
-  sm_config_set_in_pins(&c, data_pin_base);
-  sm_config_set_sideset_pins(&c, clock_pin_base);
-  //sm_config_set_sideset_pin_base(&c, clock_pin_base);
-
-  sm_config_set_in_shift(&c, true, true, 32);
-
-  float div = (float)clock_get_hz(clk_sys) * (NESPAD_CLOCK_PERIOD_S) / 16;
-  sm_config_set_clkdiv(&c, div);
-
-  printf("pio_sm_init\n");
-  pio_sm_init(pio, sm, offset, &c);
-
-  const uint pin_mask = (3u << clock_pin_base) | (3u << data_pin_base);
-  const uint pin_dirs = (3u << clock_pin_base);
-  pio_sm_set_pindirs_with_mask(pio, sm, pin_dirs, pin_mask);
-  pio_sm_set_pins(pio, sm, 0); // clear pins
-
-  // Set the state machine running
-  pio_sm_set_enabled(pio, sm, true);
-
-  printf("Adding ISR handler\n");
-
-  // irq_set_exclusive_handler(PIO0_IRQ_0, nespad_isr);
-  irq_add_shared_handler(PIO0_IRQ_0, nespad_isr, PICO_SHARED_IRQ_HANDLER_DEFAULT_ORDER_PRIORITY);
-  irq_set_enabled(PIO0_IRQ_0, true);
-  pio_set_irq0_source_enabled(pio, pis_sm0_rx_fifo_not_empty + sm, true);    
-}
 
 int main() {
   stdio_uart_init();
@@ -86,6 +25,6 @@ int main() {
 
   while(true) {
     sleep_ms(40);
-    printf("nespad_state = %32.32lb\n", _nespad_state);
+    printf("nespad_state = %32.32lb\n", nespad_state());
   }
 }
--- src/nespad.h
+++ src/nespad.h
@@ -1,39 +1,9 @@
 #pragma once
 
-#if !PICO_NO_HARDWARE
 #include "hardware/pio.h"
-#include "hardware/clocks.h"
-#endif
+#include "hardware/gpio.h"
 
-// ------ //
-// nespad //
-// ------ //
+void nespad_program_init(PIO pio, uint sm, uint data_pin_base, uint clock_pin_base);
 
-#define nespad_wrap_target 0
-#define nespad_wrap 5
+uint32_t nespad_state();
 
-static const uint16_t nespad_program_instructions[] = {
-            //     .wrap_target
-    0xb742, //  0: nop                    side 2 [7] 
-    0xb742, //  1: nop                    side 2 [7] 
-    0xe52f, //  2: set    x, 15           side 0 [5] 
-    0x4102, //  3: in     pins, 2         side 0 [1] 
-    0xaf42, //  4: nop                    side 1 [7] 
-    0x0543, //  5: jmp    x--, 3          side 0 [5] 
-            //     .wrap
-};
-
-#if !PICO_NO_HARDWARE
-static const struct pio_program nespad_program = {
-    .instructions = nespad_program_instructions,
-    .length = 6,
-    .origin = -1,
-};
-
-static inline pio_sm_config nespad_program_get_default_config(uint offset) {
-    pio_sm_config c = pio_get_default_sm_config();
-    sm_config_set_wrap(&c, offset + nespad_wrap_target, offset + nespad_wrap);
-    sm_config_set_sideset(&c, 2, false, false);
-    return c;
-}
-#endif
